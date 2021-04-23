062b962def900ad672fc963ecaf4f325ce5d6eb8
Lokesh <i.lokesh@gmail.com>
2021-04-23 17:27:53 +0530

    skip-gram vs CBOW

---
+++ NLPwithML/skip-gram.py
@@ -0,0 +1,39 @@
+
+
+import tensorflow as tf
+
+# Skip-gram embedding model
+class EmbeddingModel(object):
+    # Model Initialization
+    def __init__(self, vocab_size, embedding_dim):
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=self.vocab_size)
+
+    def tokenize_text_corpus(self, texts):
+        self.tokenizer.fit_on_texts(texts)
+        sequences = self.tokenizer.texts_to_sequences(texts)
+        return sequences
+
+    # Convert a list of text strings into word sequences
+    def get_target_and_context(self, sequence, target_index, window_size):
+        target_word = sequence[target_index]
+        half_window_size = window_size // 2
+        left_incl = max(0, target_index - half_window_size)
+        right_excl = min(len(sequence), target_index + half_window_size + 1)
+        return target_word, left_incl, right_excl
+    
+    # Create (target, context) pairs for a given window size
+    def create_target_context_pairs(self, texts, window_size):
+        pairs = []
+        sequences = self.tokenize_text_corpus(texts)      
+        for sequence in sequences:
+            for i in range(len(sequence)):
+                target_word, left_incl, right_excl = self.get_target_and_context(
+                    sequence, i, window_size)
+                # CODE HERE
+                for j in range(left_incl, right_excl):
+                    if j != i:
+                        pairs.append((target_word, sequence[j]))
+        return pairs
+
