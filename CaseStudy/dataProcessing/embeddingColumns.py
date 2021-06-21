fe38346688c9d49226e4cb10a2920f014bdfcf22
Lokesh <i.lokesh@gmail.com>
2021-06-21 17:18:18 +0530

    Spelling Mistakes...

--- CaseStudy/dataProcessing/embeddingColumns.py
+++ CaseStudy/dataProcessing/embeddingColumns.py
@@ -18,8 +18,7 @@ def add_embedding_columns(final_dataset, feature_columns):
         vocab_list = list(final_dataset[feature_name].unique())
         vocab_col = tf.feature_column.categorical_column_with_vocabulary_list(
             feature_name, vocab_list, dtype=tf.int64)
-        embedding_dim = int(vocab_list ** 0.25) # 4th root (rule of thumb)
-        feature_col = tf.feature_column.embedding_column(vocab_feature_col, embedding_dim)
+        embedding_dim = int(len(vocab_list) ** 0.25) # 4th root (rule of thumb)
+        feature_col = tf.feature_column.embedding_column(vocab_col, embedding_dim)
         feature_columns.append(feature_col)
-
 
