0da5152b76b8ad5db33beaf630150a8ed79a96f5
Lokesh <i.lokesh@gmail.com>
2021-06-21 17:14:34 +0530

    Embedding Columns...

---
+++ CaseStudy/dataProcessing/embeddingColumns.py
@@ -0,0 +1,25 @@
+
+import tensorflow as tf
+
+stores = list(range(1, 46))
+stores_col = tf.feature_column.categorical_column_with_vocabulary_list(
+    'StoreID', stores, dtype=tf.int64)
+embedding_dim = int(45**0.25)  # 4th root (Rule of thumb)
+feature_col = tf.feature_column.embedding_column(
+    stores_col, embedding_dim)
+
+## 
+
+# Add the embedding feature columns to the list of feature columns
+def add_embedding_columns(final_dataset, feature_columns):
+    embedding_features = ['Store', 'Dept']
+    for feature_name in embedding_features:
+        # CODE HERE
+        vocab_list = list(final_dataset[feature_name].unique())
+        vocab_col = tf.feature_column.categorical_column_with_vocabulary_list(
+            feature_name, vocab_list, dtype=tf.int64)
+        embedding_dim = int(vocab_list ** 0.25) # 4th root (rule of thumb)
+        feature_col = tf.feature_column.embedding_column(vocab_feature_col, embedding_dim)
+        feature_columns.append(feature_col)
+
+
